7796217f2d943137ec0d5efba4dda190ef325af3
Carlos Bernardino <carlos.bernardino@itsector.pt>
2022-01-13 11:04:45 +0000

    change to show image on consult

--- Consult Repos/View Controllers/Consult/ConsultCell.swift
+++ Consult Repos/View Controllers/Consult/ConsultCell.swift
@@ -12,19 +12,19 @@ final class ConsultCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     
     func configure(with repos: Repos) {
-        let fileUrl = URL(fileURLWithPath: repos.owner.repoImageURL)
-        self.loadImage(url:fileUrl)
+        let fileUrl = URL(string: repos.owner.repoImageURL)!
+        repoImageView.loadImage(url:fileUrl)
         nameLabel.text = repos.name
     }
 }
 
-extension ConsultCell {
+extension UIImageView {
     func loadImage(url: URL) {
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url) {
                 if let image = UIImage(data: data) {
                     DispatchQueue.main.async {
-                        self?.repoImageView.image = image
+                        self?.image = image
                     }
                 }
             }
